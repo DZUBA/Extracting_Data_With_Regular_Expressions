c524aa4cb75e373e3824f9a8f928d6336046abb1
DZUBA <zzzdzuba@gmail.com>
2016-06-03 08:40:06 +0300

    Finished work

--- code.py
+++ code.py
@@ -1,12 +1,16 @@
 import re
-hand = open('regex_sum_42.txt')
+hand = open('regex_sum_258025.txt')
 numlist = list()
+final_list = list()
 numbers_sum = 0
 for line in hand:
     line = line.rstrip()
     stuff = re.findall('[0-9]+', line)
-    #if len(stuff) == 0: continue
-    num = int(stuff[0])
+    if len(stuff) == 0: continue
+    #num = int(stuff[0])
     #numbers_sum += int(stuff)
-    numlist.append(num)
-print len(numlist)
+    numlist.append(stuff)
+for i in numlist:
+    for n in i:
+        final_list.append(int(n))
+print sum(final_list)
